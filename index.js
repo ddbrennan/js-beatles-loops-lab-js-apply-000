9071dae37ead941dd00a7cfdf6af8781bd0a3b00
David Brennan <ddbren@gmail.com>
2017-10-18 04:29:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var band = []
   for (let i = 0, l = musicians.length; i < l; i++) {
-    band[i] = `${musicians[i]} plays ${instruments[i]}`
+    band.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return band
 }
